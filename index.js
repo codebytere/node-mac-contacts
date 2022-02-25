a6d1225bae5fbbcdaeda33b0ef74727325f764f5
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2022-02-26 01:05:22 +0530

    refactor args validation

--- index.js
+++ index.js
@@ -62,91 +62,43 @@ function getContactsByName(name, extraProperties = []) {
   return contacts.getContactsByName.call(this, name, extraProperties)
 }
 
-function addNewContact(contact) {
+function validateContactArg(contact) {
   if (!contact || Object.keys(contact).length === 0) {
     throw new TypeError('contact must be a non-empty object')
-  } else {
-    const hasFirstName = contact.hasOwnProperty('firstName')
-    const hasLastName = contact.hasOwnProperty('lastName')
-    const hasNickname = contact.hasOwnProperty('nickname')
-    const hasBirthday = contact.hasOwnProperty('birthday')
-    const hasPhoneNumbers = contact.hasOwnProperty('phoneNumbers')
-    const hasEmailAddresses = contact.hasOwnProperty('emailAddresses')
+  }
 
-    if (hasFirstName && typeof contact.firstName !== 'string') {
-      throw new TypeError('firstName must be a string')
+  for (const prop of ['firstName', 'lastName', 'nickname']) {
+    const hasProp = contact.hasOwnProperty(prop)
+    if (hasProp && typeof contact[prop] !== 'string') {
+      throw new TypeError(`${prop} must be a string`)
     }
-
-    if (hasLastName && typeof contact.lastName !== 'string') {
-      throw new TypeError('lastName must be a string')
-    }
-
-    if (hasNickname && typeof contact.nickname !== 'string') {
-      throw new TypeError('nickname must be a string')
-    }
-
-    if (hasPhoneNumbers && !Array.isArray(contact.phoneNumbers)) {
-      throw new TypeError('phoneNumbers must be an array')
-    }
-
-    if (hasEmailAddresses && !Array.isArray(contact.emailAddresses)) {
-      throw new TypeError('emailAddresses must be an array')
-    }
-
-    if (hasBirthday) {
-      const datePattern = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/
-      if (typeof contact.birthday !== 'string') {
-        throw new TypeError('birthday must be a string')
-      } else if (!contact.birthday.match(datePattern)) {
-        throw new Error('birthday must use YYYY-MM-DD format')
-      }
+  }
+  for (const prop of ['phoneNumbers', 'emailAddresses']) {
+    const hasProp = contact.hasOwnProperty(prop)
+    if (hasProp && !Array.isArray(contact[prop])) {
+      throw new TypeError(`${prop} must be an array`)
     }
   }
 
+  const hasBirthday = contact.hasOwnProperty('birthday')
+
+  if (hasBirthday) {
+    const datePattern = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/
+    if (typeof contact.birthday !== 'string') {
+      throw new TypeError('birthday must be a string')
+    } else if (!contact.birthday.match(datePattern)) {
+      throw new Error('birthday must use YYYY-MM-DD format')
+    }
+  }
+}
+
+function addNewContact(contact) {
+  validateContactArg(contact)
   return contacts.addNewContact.call(this, contact)
 }
 
 function updateContact(contact) {
-  if (!contact || Object.keys(contact).length === 0) {
-    throw new TypeError('contact must be a non-empty object')
-  } else {
-    const hasFirstName = contact.hasOwnProperty('firstName')
-    const hasLastName = contact.hasOwnProperty('lastName')
-    const hasNickname = contact.hasOwnProperty('nickname')
-    const hasBirthday = contact.hasOwnProperty('birthday')
-    const hasPhoneNumbers = contact.hasOwnProperty('phoneNumbers')
-    const hasEmailAddresses = contact.hasOwnProperty('emailAddresses')
-
-    if (hasFirstName && typeof contact.firstName !== 'string') {
-      throw new TypeError('firstName must be a string')
-    }
-
-    if (hasLastName && typeof contact.lastName !== 'string') {
-      throw new TypeError('lastName must be a string')
-    }
-
-    if (hasNickname && typeof contact.nickname !== 'string') {
-      throw new TypeError('nickname must be a string')
-    }
-
-    if (hasPhoneNumbers && !Array.isArray(contact.phoneNumbers)) {
-      throw new TypeError('phoneNumbers must be an array')
-    }
-
-    if (hasEmailAddresses && !Array.isArray(contact.emailAddresses)) {
-      throw new TypeError('emailAddresses must be an array')
-    }
-
-    if (hasBirthday) {
-      const datePattern = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/
-      if (typeof contact.birthday !== 'string') {
-        throw new TypeError('birthday must be a string')
-      } else if (!contact.birthday.match(datePattern)) {
-        throw new Error('birthday must use YYYY-MM-DD format')
-      }
-    }
-  }
-
+  validateContactArg(contact)
   return contacts.updateContact.call(this, contact)
 }
 
